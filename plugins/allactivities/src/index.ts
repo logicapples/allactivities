415790adf4a4557e0cd6c0192ca77e1fca237956
Blaž <76408169+blaz6@users.noreply.github.com>
2024-02-04 02:25:05 +0100

    hm

--- plugins/allactivities/src/index.ts
+++ plugins/allactivities/src/index.ts
@@ -1,12 +1,40 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { logger, patcher } from "@vendetta";
 
-export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+const unpatch = before("openLazy", LazyActionSheet, ([component, key, msg]) => {
+    const message = msg?.message
+    if (key !== "MessageLongPressActionSheet" || !message) return
+    component.then(instance => {
+        const unpatch = after("default", instance, (_, component) => {
+            React.useEffect(() => () => { unpatch() }, [])
+            const buttons = findInReactTree(component, x => x?.[0]?.type?.name === "ButtonRow")
+            if (!buttons) return
+
+            const navigator = () => (
+                <Navigator
+                    initialRouteName="RawPage"
+                    goBackOnBackPress
+                    screens={{
+                        RawPage: {
+                            title: "ViewRaw",
+                            headerLeft: modalCloseButton?.(() => Navigation.pop()),
+                            render: () => <RawPage message={message} />
+                        }
+                    }}
+                />
+            )
+
+            buttons.push(
+                <FormRow
+                    label="View Raw"
+                    leading={<FormIcon style={{ opacity: 1 }} source={getAssetIDByName("ic_chat_bubble_16px")} />}
+                    onPress={() => {
+                        LazyActionSheet.hideActionSheet()
+                        Navigation.push(navigator)
+                    }}
+                />
+            )
+        })
+    })
+})
+
+export const onUnload = () => unpatch()
